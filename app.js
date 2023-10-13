2ae1655fd77558e115c64e29b22ff39d93619848
JovHariono <128022304+JovHariono@users.noreply.github.com>
2023-10-13 21:34:12 +0700

    buat simpan kontak dan bljr menggunakan library2

---
+++ app.js
@@ -0,0 +1,62 @@
+//CODING LAMA
+// rl.question("Masukkan nama anda : ", (nama) => {
+//   rl.question("Masukkan no hp anda : ", (hp) => {
+//     const contact = { nama, hp };
+
+//     const file = fs.readFileSync("./data/contacts.json", "utf-8");
+//     const contacts = JSON.parse(file);
+
+//     contacts.push(contact);
+
+//     fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+
+//     console.log("Terima kasih sudah memasukkan data kamu");
+
+//     rl.close();
+//   });
+// });
+
+//CODING APP (10)
+// const contacts = require("./contacts");
+
+// const main = async () => {
+//   const nama = await contacts.pertanyaan("Masukkan nama anda : ");
+//   const email = await contacts.pertanyaan("Masukkan email anda : ");
+//   const nomor = await contacts.pertanyaan("Masukkan nomor anda : ");
+
+//   contacts.simpanContact(nama, email, nomor)
+// };
+
+// main();
+
+//CODING AMBIL ARGUMEN DARI COMMAND LINE
+const yargs = require("yargs");
+const contact = require("./contacts")
+
+yargs.command({
+  command: "add",
+  describe: "Menambahkan kontak baru",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: 'Email',
+      demandOption: true,
+      type: 'string'
+    },
+    nomor: {
+      describe: 'Nomor Hp',
+      demandOption: true,
+      type: 'stringg'
+    }
+  },
+  
+  handler(argv){
+   contact.simpanContact( argv.nama, argv.email, argv.nomor )
+  }
+});
+
+yargs.parse();
---
+++ contacts.js
@@ -0,0 +1,113 @@
+const { rejects } = require("assert");
+const fs = require("fs");
+const { resolve } = require("path");
+const chalk = require("chalk");
+const validator = require("validator");
+
+//MENULISKAN STRING KE FILE
+// try {
+//   fs.writeFileSync("data/test.tsx", "Hellow World secara synchronus!");
+// } catch (e) {
+//     console.log(e)
+// }
+
+//MENULISKAN STRING KE FILE (ASYNCHRONUS)
+// fs.writeFile('data/test.txt', 'Hello world secara Asynchronus!', (e) => {
+//     console.log(e)
+// })
+
+//MEMBACA FILE
+// const data = fs.readFileSync('data/test.txt', 'utf-8')
+// console.log(data)
+
+//MEMBACA FILE (ASYNCHRONUS)
+// fs.readFile('data/test.txt', 'utf-8', (e, data)=>{
+//     if (e) throw e
+//     console.log(data)
+// })
+
+//Readline
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+//membuat folder data
+if (!fs.existsSync("./data")) {
+  fs.mkdirSync("./data");
+}
+
+//membuat file contacs.json jiak blm ada
+if (!fs.existsSync("./data/contacts.json")) {
+  fs.writeFileSync("./data/contacts.json", "[]", "utf-8");
+}
+
+// const pertanyaan = (tanya) => {
+//   return new Promise((resolve, reject) => {
+//     rl.question(tanya, (data) => {
+//       resolve(data);
+//     });
+//   });
+// };
+
+// const pertanyaan2 = () => {
+//   return new Promise((resolve, reject) => {
+//     rl.question("Masukkan email anda: ", (email) => {
+//       resolve(email);
+//     });
+//   });
+// };
+
+// const pertanyaan3 = () => {
+//   return new Promise((resolve, reject) => {
+//     rl.question("Masukkan nomor anda: ", (nomor) => {
+//       resolve(nomor);
+//     });
+//   });
+// };
+
+const simpanContact = (nama, email, nomor) => {
+  const contact = { nama, email, nomor };
+
+  const file = fs.readFileSync("./data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
+
+  //cek duplikat
+  const duplikat = contacts.find((contact) => contact.nama === nama);
+
+  if (duplikat) {
+    console.log(
+      chalk.red.inverse.bold("Kontak sudah terdaftar, gunakan nama lainnya!")
+    );
+    return false;
+  }
+
+  //cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(
+        chalk.red.inverse.bold("Email yang anda masukukan tidak valid!")
+      );
+      return false;
+    }
+  }
+
+  //cek nomor
+  if (nomor) {
+    if (!validator.isMobilePhone(nomor, "id-ID")) {
+      console.log(
+        chalk.red.inverse.bold("Nomor yang anda masukukan tidak valid!")
+      );
+      return false;
+    }
+  }
+
+  contacts.push(contact);
+
+  fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+
+  console.log(chalk.green.inverse.bold("Terima kasih sudah memasukkan data kamu"));
+};
+
+module.exports = { simpanContact };
